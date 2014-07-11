9c5ae0b32e6a88d852b3e4c0baf5b21c1b3f16ba
Matt Adelberger <matt.adelberger@gmail.com>
2014-07-11 14:04:09 -0500

    Added Instagram and Bands In Town Support

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,57 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require turbolinks
+//= require jquery-dateFormat
 //= require_tree .
+
+$(document).ready(function() {
+
+	var instagramPhotos = [];
+	var shows = [];
+
+	$.ajax({
+	    url: 'https://api.instagram.com/v1/users/1140395903/media/recent?access_token=1140395903.467ede5.08dc5e6254c249bc850c26c1ebf00a37',
+	    type: 'GET',
+	    dataType: 'jsonp',
+	    success: function(result) {
+	    	instagramPhotos = result.data;
+
+	    	var img = $('<img class="col-lg-12">');
+	    	img.attr('src', instagramPhotos[0].images.standard_resolution.url);
+	    	$('#newestInstagram').html(img);
+
+	    	$.each(instagramPhotos, function(key, val) {
+	    		if( key === 12) {
+	    			return false;
+	    		}
+	    		var img = $('<div class="col-lg-3"><img width="100%" height="100%" src=' 
+	    			+ instagramPhotos[key].images.standard_resolution.url 
+	    			+ '></div>');
+	    		img.appendTo('#instagramFeed');
+	    	})
+	    }
+	});
+
+	$.ajax({
+	    url: 'http://api.bandsintown.com/artists/cowboyjukebox/events.json?api_version=2.0&app_id=COWBOYJUKEBOX',
+	    type: 'GET',
+	    dataType: 'jsonp',
+	    success: function(result) {
+	    	shows = result;
+
+	    	$.each(shows, function(key, val) {
+
+	    		var formattedDate = $.format.date(val.datetime, "MMM d")
+	    		var formattedTime = $.format.date(val.datetime, "h:mm a")
+
+
+	    		$("#tour > tbody").append("<tr><td>"
+	    			+ formattedDate + "</td><td>" 
+	    			+ val.venue.name + "</td><td>"
+	    			+ val.venue.city + ", " + val.venue.region + "</td><td>"
+	    			+ formattedTime + "</td><tr>");
+	    	})
+	    }
+	});
+});
+
