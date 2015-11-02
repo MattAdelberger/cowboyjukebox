7991a2ba362632c46ea28a0be7cdc33c823f9dac
Matt Adelberger <matt.adelberger@gmail.com>
2015-11-02 10:53:00 -0600

    updated how tour dates are displayed

--- public/js/tour-dates.js
+++ public/js/tour-dates.js
@@ -1,32 +1,50 @@
 $(document).ready(function() {
 
+	var upcomingCount = 0;
+
 	$.ajax({
-	    url: 'http://api.bandsintown.com/artists/cowboyjukebox/events.json?api_version=2.0&app_id=COWBOYJUKEBOX',
+	    url: 'http://api.bandsintown.com/artists/cowboyjukebox/events.json?api_version=2.0&app_id=COWBOYJUKEBOX&date=upcoming',
 	    type: 'GET',
 	    dataType: 'jsonp',
 	    success: function(result) {
-	    	allUpcomingShows = result;
+	    	allUpcomingShows = result.reverse();
 
 	    	$.each(allUpcomingShows, function(key, val) {
 
-	    		var formattedDate = $.format.date(val.datetime, "MMM d")
+	    		upcomingCount = upcomingCount + 1;
+
+	    		var formattedDate = $.format.date(val.datetime, "MMM d yyyy")
 	    		var formattedTime = $.format.date(val.datetime, "h:mm a")
 
-	    		$("#allShows > tbody").append("<tr><td>"
-	    			+ formattedDate + "</td><td>" 
-	    			+ val.venue.name + "</td><td>"
-	    			+ val.venue.city + ", " + val.venue.region + "</td><td>"
-	    			+ formattedTime + "</td><tr>");
-
-	    		if(key < 4) {
-	    			$("#upcomingShows > tbody").append("<tr><td>"
-	    			+ formattedDate + "</td><td>" 
-	    			+ val.venue.name + "</td><td>"
-	    			+ val.venue.city + ", " + val.venue.region + "</td><td>"
-	    			+ formattedTime + "</td><tr>");
-	    		}
+    			$("#upcomingShows > tbody").append("<tr><td>"
+    			+ formattedDate + "</td><td>" 
+    			+ val.venue.name + "</td><td>"
+    			+ val.venue.city + ", " + val.venue.region + "</td><td>"
+    			+ formattedTime + "</td><tr>");
 	    	})
 	    }
 	});
 
+	$.ajax({
+	    url: 'http://api.bandsintown.com/artists/cowboyjukebox/events.json?api_version=2.0&app_id=COWBOYJUKEBOX&date=all',
+	    type: 'GET',
+	    dataType: 'jsonp',
+	    success: function(result) {
+	    	allUpcomingShows = result.reverse();
+
+	    	$.each(allUpcomingShows, function(key, val) {
+
+	    		var formattedDate = $.format.date(val.datetime, "MMM d yyyy")
+	    		var formattedTime = $.format.date(val.datetime, "h:mm a")
+
+	    		if(key > upcomingCount) {
+		    		$("#allShows > tbody").append("<tr><td>"
+		    			+ formattedDate + "</td><td>" 
+		    			+ val.venue.name + "</td><td>"
+		    			+ val.venue.city + ", " + val.venue.region + "</td><td>"
+		    			+ formattedTime + "</td><tr>");
+	    		}
+	    	})
+	    }
+	});
 });
